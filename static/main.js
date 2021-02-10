0112afc261a278e5b9309b635ce5b8f5a3da10bf
kashifcap <34791492+kashifcap@users.noreply.github.com>
2021-02-10 23:43:17 +0530

    Added local storage and made it responsive

--- static/main.js
+++ static/main.js
@@ -1,8 +1,7 @@
-// Selectors
-
 state = {
   bookList: [],
 };
+// Selectors
 
 const form = document.querySelector(".bookform");
 const titleInput = document.getElementById("book-name");
@@ -11,6 +10,7 @@ const codeInput = document.getElementById("book-id");
 const messageBox = document.querySelector(".message");
 const bookSectionContainer = document.querySelector(".book-list");
 const bookListCOntainer = document.querySelector(".item-container");
+const removeBtn = document.querySelector(".remove");
 
 const clearInputs = function () {
   titleInput.value = "";
@@ -37,28 +37,64 @@ form.addEventListener("submit", function (e) {
   const title = titleInput.value;
   const author = authorInput.value;
   const isbn = codeInput.value;
-  state.bookList.push({
+  addItem({
     title,
     author,
     isbn,
   });
   clearInputs();
-  reset();
-  loadList();
-  messageAlert("Booklist Added", "success");
+  render();
+  messageAlert("Book Added", "success");
 });
 
-const reset = function () {
-  if (state.bookList.length) {
-    bookSectionContainer.classList.remove("hidden");
-    bookListCOntainer.innerHTML = "";
-  }
+bookListCOntainer.addEventListener("click", function (e) {
+  const btn = e.target.closest(".remove");
+  if (!btn) return;
+  const id = btn.parentElement.dataset.id;
+  const el = state.bookList.filter((item) => item.id === id)[0];
+  deleteItem(el);
+  render();
+  messageAlert("Book Removed", "danger");
+});
+
+const addItem = function (item) {
+  const id = (Date.now() + "").slice(-10);
+  item = {
+    id,
+    title: item.title,
+    author: item.author,
+    isbn: item.isbn,
+  };
+  state.bookList.push(item);
+  localStorage.setItem("booklist", JSON.stringify(state.bookList));
+};
+
+const deleteItem = function (item) {
+  state.bookList.splice(state.bookList.indexOf(item), 1);
+  localStorage.setItem("booklist", JSON.stringify(state.bookList));
 };
 
 const loadList = function () {
+  const mylist = localStorage.getItem("booklist");
+  if (!mylist) return;
+  state.bookList = JSON.parse(mylist);
+};
+
+const render = function () {
+  if (state.bookList.length) {
+    bookSectionContainer.classList.remove("hidden");
+    bookListCOntainer.innerHTML = "";
+    renderlist();
+  } else {
+    bookListCOntainer.innerHTML = "";
+    bookSectionContainer.classList.add("hidden");
+  }
+};
+
+const renderlist = function () {
   const markup = state.bookList
     .map((book) => {
-      const mark = `<div class="item">
+      const mark = `<div class="item" data-id="${book.id}">
             <h2 class="name">${book.title}</h2>
             <h2 class="author">${book.author}</h2>
             <h2 class="id">${book.isbn}</h2>
@@ -72,10 +108,9 @@ const loadList = function () {
   bookListCOntainer.insertAdjacentHTML("afterbegin", markup);
 };
 
-const renderlist = function () {};
-
 const init = function () {
   loadList();
-  reset();
-  renderlist();
+  render();
 };
+
+init();
